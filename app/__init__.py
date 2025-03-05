9294d59343e4ddc399bdeeafef9b5a4469ffb817
Vio1et <101125673+Iateautumn@users.noreply.github.com>
2025-03-05 16:43:07 +0800

    a more specific project structure with a runnable website backend example, and an example of using PR
    * initialization of login and register branch
    
    * ideal backend structure of auth part
    
    * connect database
    
    * move database connection code from __init__.py to extensions.py
    
    ---------
    
    Co-authored-by: Levine <2987471699@qq.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,45 +1,38 @@
-# from flask import Flask, redirect, url_for
-# from flask_login import current_user
+from flask import Flask, redirect, url_for, request
+from flask_login import current_user
+from app.auth.routes import auth_bp
+from app.extensions import db, login_manager, init_db
 
+def create_app():
+    app = Flask(__name__)
+    init_db(app)
 
+    app.config['SECRET_KEY'] = 'your-secret-key-here'
 
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
+    app.register_blueprint(auth_bp)
 
-# app
+    @app.route('/')
+    def root_redirect():
+        if current_user.is_authenticated:
+            return redirect(url_for('booking.dashboard'))
+        return redirect(url_for('auth.login'))
 
-# def create_app():
-#     app = Flask(__name__)
+    @app.before_request
+    def check_authentication():
 
-#     @app.route('/')
-#     def root_redirect():
-#         if current_user.is_authenticated:
-#             return redirect(url_for('booking.dashboard'))
-#         return redirect(url_for('auth.login'))
+        if request.path.startswith('/static'):
+            return
+        if request.endpoint in ['auth.login', 'auth.register']:
+            return
 
-#     return app
+        if not current_user.is_authenticated:
+            return redirect(url_for('auth.login', next=request.full_path))
 
+    return app
 
-
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import json
-
-with open('config.json', 'r') as f:
-    data = json.load(f)
-
-database_config = data['database']
-database_url = database_config['url']
-database_port = database_config['port']
-database_username = database_config['username']
-database_password = database_config['password']
-database_name = database_config['database_name']
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{database_username}:{database_password}@{database_url}:{database_port}/{database_name}?charset=utf8'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-db = SQLAlchemy(app)
-
-from app.auth import models
-from app.booking import models
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
