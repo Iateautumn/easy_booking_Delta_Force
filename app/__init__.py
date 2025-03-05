da215f283cfcd8c86bda6daddb32410a73c29977
lVio1etl <624485045@qq.com>
2025-03-05 16:09:03 +0800

    move database connection code from __init__.py to extensions.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,48 +1,29 @@
-from flask import Flask, redirect, url_for, request  # 导入 request
-from flask_login import current_user, LoginManager    # 导入 LoginManager
+from flask import Flask, redirect, url_for, request
+from flask_login import current_user
 from app.auth.routes import auth_bp
-from app.extensions import login_manager, db
-import json
+from app.extensions import db, login_manager, init_db
 
 def create_app():
     app = Flask(__name__)
 
+    init_db(app)
 
-    with open('config.json', 'r') as f:
-        data = json.load(f)
+    app.config['SECRET_KEY'] = 'your-secret-key-here'
 
-    database_config = data['database']
-    database_url = database_config['url']
-    database_port = database_config['port']
-    database_username = database_config['username']
-    database_password = database_config['password']
-    database_name = database_config['database_name']
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{database_username}:{database_password}@{database_url}:{database_port}/{database_name}?charset=utf8'
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
-    app.config['SECRET_KEY'] = 'your-secret-key-here'  # 设置密钥
-
-    # 初始化 Flask-Login
-    # login_manager = LoginManager()
-    # login_manager.init_app(app)
-    # login_manager.login_view = 'auth.login'  # 指定登录视图端点
-    db.init_app(app)
-    login_manager.init_app(app)  # 初始化 Flask-Login
-    login_manager.login_view = 'auth.login'  # 设置登录端点
-    # 先注册蓝图
     app.register_blueprint(auth_bp)
 
-    # 根路由
     @app.route('/')
     def root_redirect():
         if current_user.is_authenticated:
             return redirect(url_for('booking.dashboard'))
         return redirect(url_for('auth.login'))
 
-    # 全局认证检查钩子
     @app.before_request
     def check_authentication():
-        # 排除静态文件和认证相关路由
+
         if request.path.startswith('/static'):
             return
         if request.endpoint in ['auth.login', 'auth.register']:
