4d4f51de5f9fa9e0f08c069179bcbf979b7128ed
lVio1etl <624485045@qq.com>
2025-03-06 15:33:30 +0800

    A rough version of the login and registration functionality.

--- app/auth/services.py
+++ app/auth/services.py
@@ -1,20 +1,52 @@
 # app/auth/services.py
-from werkzeug.security import check_password_hash, generate_password_hash
-from app.auth.models import User
-import app.auth.models as Models
-from app.extensions import db
-from app.auth.exceptions import BusinessError
+from werkzeug.security import generate_password_hash
+from app.auth.models import User, add_user
+from app.utils.exceptions import BusinessError
+from flask import current_app
+import os
+import hashlib
 
-def register_user(username, email, password):
-    password_hash = generate_password_hash(password)
-    user = User(username=username, email=email, password_hash=password_hash)
-    db.session.add(user)
-    db.session.commit()
+def generate_password_hash(password, salt=None):
+
+    if salt is None:
+        salt = os.urandom(32).hex()
+
+    password_with_salt = password + salt
+
+    hashed_password = hashlib.sha256(password_with_salt.encode()).hexdigest()
+    return hashed_password, salt
+
+def register_user(status, username, email, password):
+
+    password_hash, salt = generate_password_hash(password)
+
+    try:
+        user = add_user(status, username, email, password_hash, salt)
+    except Exception as e:
+        raise BusinessError('Failed to register user', 500)
+
     return user
 
-def login_user(email, password):
-    user = User.query.filter_by(email=email).first()
-    if user and check_password_hash(user.password_hash, password):
+def login_user(username, password):
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        raise BusinessError('Invalid username or password', 401)
+
+    salt = user.salt
+    password_hash = user.password_hash
+
+    hashed_password = current_app.pbkdf2_hmac(
+        'sha256',
+        password.encode('utf-8'),
+        bytes.fromhex(salt),
+        100000
+    )
+
+    hashed_password_hex = hashed_password.hex()
+
+    if password_hash == hashed_password_hex:
         return user
-    raise BusinessError('Invalid username or password', 401)
-    return None
+    else:
+        raise BusinessError('Invalid username or password', 401)
---
+++ app/utils/exceptions.py
@@ -0,0 +1,4 @@
+class BusinessError(Exception):
+    def __init__(self, message, code = 400):
+        self.message = message
+        self.code = code
