cf88cf945917288b36674b1ac45861e648df05ad
lVio1etl <624485045@qq.com>
2025-03-06 23:25:09 +0800

    tiny modification

--- app/booking/routes.py
+++ app/booking/routes.py
@@ -1,15 +1,23 @@
 # app/classrooms/routes.py
-from flask import Blueprint, request, jsonify, redirect, url_for
+from flask import Blueprint, request, jsonify, redirect, url_for, render_template
 from flask_login import current_user
+from app.booking.booking_room import bookings
 from app.utils.response import error_response, success_response
 from app.utils.exceptions import BusinessError
 
 from app.booking.services import new_booking,filter_classrooms
-import app.booking.services as Services
 
-classroom_bp = Blueprint('booking', __name__,url_prefix='/booking')
 
-@classroom_bp.route('/new', methods=['POST'])
+booking_bp = Blueprint('booking', __name__, url_prefix='/booking')
+
+@booking_bp.route('/dashboard', methods=['GET'])
+def dashboard():
+    if not current_user.is_authenticated:
+        return redirect(url_for('auth.login'))
+
+    return render_template('/booking/dashboard.html')
+
+@booking_bp.route('/new', methods=['POST'])
 def list_classrooms():
 
     if not current_user.is_authenticated:
@@ -20,11 +28,13 @@ def list_classrooms():
     except Exception as e:
         return error_response("bad request: " + str(e), 400)
 
+    user_id = current_user.id
     room_id = data['room_id']
     time_period = data['time_period']
+    date = data['date']
 
     try:
-        new_booking(room_id, time_period)
+        new_booking(user_id, room_id, time_period, date)
         return success_response("success reservation")
     except BusinessError as e:
         return error_response(str(e), e.code)
--- app/booking/services.py
+++ app/booking/services.py
@@ -1,13 +1,39 @@
 # app/bookings/services.py
 from datetime import datetime
+from lib2to3.fixer_util import touch_import
+
 from sqlalchemy import and_
 from werkzeug.http import parse_age
 from models import add_reservation
-
+from datetime import datetime, timedelta
 from app.extensions import db
 from app.booking.models import Reservation as Booking
 import app.booking.models as Models
 
+time_slot_map = {
+    0: {'start': '08:00:00', 'end': '08:45:00'},
+    1: {'start': '08:45:00', 'end': '09:40:00'},
+    2: {'start': '10:00:00', 'end': '10:45:00'},
+    3: {'start': '10:55:00', 'end': '11:40:00'},
+    4: {'start': '14:00:00', 'end': '14:45:00'},
+    5: {'start': '14:55:00', 'end': '15:40:00'},
+    6: {'start': '16:00:00', 'end': '16:45:00'},
+    7: {'start': '16:55:00', 'end': '17:40:00'},
+    8: {'start': '19:00:00', 'end': '19:45:00'},
+    9: {'start': '19:55:00', 'end': '20:40:00'},
+}
+
+
+def add_time(date_str, time_str):
+
+    date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+    time_obj = datetime.strptime(time_str, '%H:%M:%S')
+
+    time_delta = timedelta(hours=time_obj.hour, minutes=time_obj.minute, seconds=time_obj.second)
+
+    combined_date = date_obj + time_delta
+
+    return combined_date
 
 class ConflictError(Exception):
     pass
@@ -17,7 +43,9 @@ class PermissionError(Exception):
     pass
 
 
-def new_booking(user_id, classroom_id, start_time, end_time):
+def new_booking(user_id, classroom_id, time_period, date):
+    start_time = add_time(date, time_slot_map[time_period]['start'])
+    end_time = add_time(date, time_slot_map[time_period]['end'])
 
     conflict = Booking.query.filter(
         (Booking.classroom_id == classroom_id) &
