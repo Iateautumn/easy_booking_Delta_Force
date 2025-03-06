83e721992cd19a8f4f9b4d7d71470ac6fd9f37a5
WSZCY <13375893+wszcy@user.noreply.gitee.com>
2025-03-06 16:59:22 +0800

    Login integration

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -9,3 +9,55 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
   container.classList.remove('active')
 })
+
+async function registerUser(username, email, password, status) {
+  const apiUrl = '/auth/register';
+  const userData = {
+    username,
+    email,
+    password,
+    status
+  };
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const errorData = await response.json();
+  if (errorData) {
+    switch (errorData.status) {
+      case 200:
+        alert('Register Success! Please login now!');
+        window.location.href = '/login';
+        break;
+        case 400:
+          throw new Error(`Validation failed: ${errorData.details?.join(', ') || 'Invalid parameters'}`);
+      case 409:
+          throw new Error(`Resource conflict: ${errorData.message || 'Duplicate username/email'}`);
+      default:
+          throw new Error(`Internal server error (${response.status})`);
+    }
+  }
+}
+
+function handleRegister() {
+  const username = document.getElementById('registration-username').value;
+  const email = document.getElementById('registration-email').value;
+  const password = document.getElementById('registration-password').value;
+  const status = document.getElementById('status').value;
+
+  if (!username || !email || !password || status === "Select Your Identity...") {
+    alert("Please fill in all fields!");
+    return;
+  }
+
+  registerUser(username, email, password, status);
+
+}
