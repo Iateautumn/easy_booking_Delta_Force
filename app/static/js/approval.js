cd6054b74f768bfff027196e4fb063d911dee079
coldsummer <134433057@qq.com>
2025-03-11 14:49:09 +0800

    add API functions in approval.js

---
+++ app/static/js/approval.js
@@ -0,0 +1,87 @@
+async function getAllBookingRequests() {
+    const apiUrl = '/admin/reservation/request';
+    const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const Data = await response.json();
+
+    if (Data) {
+        switch (Data.code) {
+            case 200:
+                return Array.isArray(Data.data) ? Data.data : [];
+            default:
+                alert(`Error, (${Data.message})`);
+                return [];
+        }
+    }
+    else {
+        alert('Error, Network Error');
+        return [];
+    }
+}
+
+async function approveBookingRequest(reservation_id) {
+    const apiUrl = '/admin/reservation/approval';
+    const userData = {
+        "reservation_id": reservation_id
+    };
+
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
+
+    const Data = await response.json();
+
+    if (Data) {
+        switch (Data.code) {
+            case 200:
+                return true;
+            default:
+                alert(`Error, (${Data.message})`);
+                return false;
+        }
+    }
+    else {
+        alert('Error, Network Error');
+        return false;
+    }
+}
+
+async function rejectBookingRequest(reservation_id) {
+    const apiUrl = '/admin/reservation/deny';
+    const userData = {
+        "reservation_id": reservation_id
+    };
+
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
+
+    const Data = await response.json();
+
+    if (Data) {
+        switch (Data.code) {
+            case 200:
+                return true;
+            default:
+                alert(`Error, (${Data.message})`);
+                return false;
+        }
+    }
+    else {
+        alert('Error, Network Error');
+        return false;
+    }
+}
