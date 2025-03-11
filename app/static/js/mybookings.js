0611fb4159764b9dc59d36e6a100b04f35d772c1
coldsummer <1534433057@qq.com>
2025-03-11 16:39:40 +0800

    add mybookings.js

---
+++ app/static/js/mybookings.js
@@ -0,0 +1,21 @@
+async function getAllMyBookings() {
+    const apiUrl = '/api/mybookings';
+    const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    
+    const data = await response.json();
+
+    if (data) {
+        switch (data.code) {
+            case 200:
+                return Array.isArray(data.data) ? data.data : [];
+            default:
+                alert(`Error, (${data.message})`);
+                return [];
+        }
+    }
+}
