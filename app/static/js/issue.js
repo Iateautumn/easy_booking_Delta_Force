3f8bc2135194fb3cab3bc62c4d587a57afcc12e4
WSZCY <13375893+wszcy@user.noreply.gitee.com>
2025-03-18 17:18:13 +0800

    Report almost finished

--- app/static/js/issue.js
+++ app/static/js/issue.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 async function getRoomIssue() {
     const apiUrl = '/admin/issue/all';
     const response = await fetch(apiUrl, {
@@ -55,33 +60,41 @@ async function removeRoomIssue(issue_id) {
     }
 }
 
-async function userReportIssue(issue){
-    const apiUrl = '/user/classroom/issue/report';
-    const userData = {
-        "issue": issue
-    };
+async function viewIssues() {
+    const issues = await getRoomIssue();
 
-    const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
+    const issueList = document.getElementById('issue-list');
+
+    if(issues.length > 0) {
+        issueList.innerHTML = '';
+        issues.forEach(issue => {
+            const issueCard = document.createElement('div');
+            issueCard.classList.add('issue-card');
+            issueCard.innerHTML = `
+                <h3>Issue ID: ${issue.issueId}</h3>
+                <p>Issue: ${issue.issue}</p>
+                <p>Reported by: ${issue.username}</p>
+                <p>Date Reported: ${issue.date}</p>
+                <button class="action-btn" id="remove-issue">Remove</button>
+            `;
+            issueList.appendChild(issueCard);
+        });
+    }
+    
+    const removeBtns = document.querySelectorAll('#remove-issue');
+    removeBtns.forEach((btn, index) => {
+        btn.addEventListener('click', async () => {
+            const issue_id = issues[index].issueId;
+            const result = await removeRoomIssue(issue_id);
+            if (result) {
+                alert('Removed');
+                viewIssues();
+            }
+            else {
+                alert('Error, Network Error');
+            }
+        });
     });
 
-    const Data = await response.json();
 
-    if (Data) {
-        switch (Data.code) {
-            case 200:
-                return true;
-            default:
-                alert(`Error, (${Data.message})`);
-                return false;
-        }
-    }
-    else {
-        alert('Error, Network Error');
-        return false;
-    }
 }
