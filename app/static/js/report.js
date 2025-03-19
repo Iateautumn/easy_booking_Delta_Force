4164a4d0cc2abbf0bb05edec7a51035dd118946b
coldsummer <134433057@qq.com>
2025-03-19 17:17:15 +0800

    add API functions

---
+++ app/static/js/report.js
@@ -0,0 +1,42 @@
+async function getReport() {
+    const apiUrl = '/admin/report/analysis';
+    const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const Data = await response.json();
+
+    if (Data) {
+        switch (Data.code) {
+            case 200:
+                return Data.data;
+            default:
+                alert(`Error, (${Data.message})`);
+                return [];
+        }
+    }
+    else {
+        alert('Error, Network Error');
+        return [];
+    }
+}
+
+async function getReportPDF() {
+    const apiUrl = '/admin/report/analysis/export';
+    await fetch(apiUrl)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = 'all_reservation.ics';
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(error => console.error('Error:', error));
+}
